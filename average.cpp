1a027756949d6f7edcc0e4f4e38950f2a5816f3d
NNNewwear <nextkantapong@gmail.com>
2024-02-27 12:19:18 +0700

    Lab22

--- average.cpp
+++ average.cpp
@@ -1,1 +1,17 @@
-
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+int main(int argc,char* argv[]){
+    int a=0,b=0;
+    if(argc == 1) cout << "Please input numbers to find average.";
+    else{
+        for(int i=1;i<argc;i++){
+            a += atoi(argv[i]);
+            b += 1;
+        }
+        cout << "---------------------------------" << endl;
+        cout << "Average of " << b << " numbers = " << a/b << endl; 
+        cout << "---------------------------------" << endl;
+    }
+    return 0;
+}
